1ee8081e5fdb9fcd20131cda74d8a350a5a26b51
Jose Samper <jmiguelss@yahoo.es>
2015-10-30 06:48:28 +0000

    Updated to 3.0.0.BETA01

--- src/main/java/com/thymeleafexamples/thymeleaf3/config/ThymeleafConfig.java
+++ src/main/java/com/thymeleafexamples/thymeleaf3/config/ThymeleafConfig.java
@@ -1,12 +1,13 @@
 package com.thymeleafexamples.thymeleaf3.config;
 
+import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ITemplateResolver;
 import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
-import org.thymeleaf.templateresolver.TemplateResolver;
 
 /**
  * Thymeleaf configuration.
@@ -15,20 +16,21 @@ import org.thymeleaf.templateresolver.TemplateResolver;
 public class ThymeleafConfig implements ServletContextListener {
 
     public void contextInitialized(ServletContextEvent sce) {
-        TemplateEngineUtil.storeTemplateEngine(sce.getServletContext(), templateEngine());
+        TemplateEngine engine = templateEngine(sce.getServletContext());
+        TemplateEngineUtil.storeTemplateEngine(sce.getServletContext(), engine);
     }
 
     public void contextDestroyed(ServletContextEvent sce) {
     }
 
-    private TemplateEngine templateEngine() {
+    private TemplateEngine templateEngine(ServletContext servletContext) {
         TemplateEngine engine = new TemplateEngine();
-        engine.addTemplateResolver(templateResolver());
+        engine.addTemplateResolver(templateResolver(servletContext));
         return engine;
     }
 
-    private TemplateResolver templateResolver() {
-        ServletContextTemplateResolver resolver = new ServletContextTemplateResolver();
+    private ITemplateResolver templateResolver(ServletContext servletContext) {
+        ServletContextTemplateResolver resolver = new ServletContextTemplateResolver(servletContext);
         resolver.setPrefix("/WEB-INF/templates/");
         resolver.setTemplateMode(TemplateMode.HTML);
         return resolver;
